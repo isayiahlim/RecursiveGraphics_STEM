6cd6c7c845af25b91c69a051c08f4d531074933b
s-ILIM <s-ILIM@487M113743.lwsd.k12wa.org>
2022-10-14 10:08:01 -0700

    Still trying to fix rotate

--- RecursiveGraphics/src/Transform2D.java
+++ RecursiveGraphics/src/Transform2D.java
@@ -115,6 +115,9 @@ public class Transform2D
 		for(int i = 0; i < x.length; i++)
 		{
 			x[i] = (x[i] * Math.cos(theta)) - (y[i] * Math.sin(theta));
+		}
+		for(int i = 0; i < y.length; i++)
+		{
 			y[i] = (y[i] * Math.cos(theta)) + (x[i] * Math.sin(theta));
 		}
 	}
